c8c5cc5a66cc836ba6e6d7b5c3cb1f89bf1ee762
Chris Hellmuth <chellmuth@crowdcompass.com>
2013-07-06 10:33:08 -0600

    Add initial Podfile

--- HelloCocoapods/HCAppDelegate.h
+++ HelloCocoapods/HCAppDelegate.h
@@ -6,6 +6,10 @@
 //  Copyright (c) 2013 Chris Hellmuth. All rights reserved.
 //
 
+// test our Pod libraries
+#import <Reachability.h>
+#import <JSONKit.h>
+
 #import <UIKit/UIKit.h>
 
 @interface HCAppDelegate : UIResponder <UIApplicationDelegate>
