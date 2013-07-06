9abc6a789dd8be4d9c21f13850b169feb26c4f72
Chris Hellmuth <chellmuth@crowdcompass.com>
2013-07-06 10:28:17 -0600

    Initial Commit

---
+++ HelloCocoapods/HCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HCAppDelegate.h
+//  HelloCocoapods
+//
+//  Created by Chris Hellmuth on 7/6/13.
+//  Copyright (c) 2013 Chris Hellmuth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HelloCocoapods/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HelloCocoapods
+//
+//  Created by Chris Hellmuth on 7/6/13.
+//  Copyright (c) 2013 Chris Hellmuth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HCAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HCAppDelegate class]));
+    }
+}
